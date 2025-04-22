6175f521d59cae5f3615deb41518fcf6ee448675
mahesh <>
2025-04-22 09:03:42 +0530

    first dart program , variable in dart

---
+++ lib/dart/first.dart
@@ -0,0 +1,12 @@
+//Basic Dart Program
+//This program prints "Hello, World!" to the console 
+
+void main() {
+  print("Hello World!");
+}
+
+// void main() -> is the starting point of the program.
+// every dart program must have a main() function.
+//The curly braces {} represent the beginning and ending of a block of code 
+//print() prints the hello world on screen
+// Each code statement must end with a semicolon (;)
---
+++ lib/dart/variable.dart
@@ -0,0 +1,41 @@
+//variables 
+/*
+var/final/const name = value;
+
+Variable are containers used to store value in the program. 
+var/final/const are used to declare variables in dart
+
+Rules for naming variables in Dart:
+. Variable names care case sensitive .  a and A are different variables
+ . A variable name can consist of letters  and alphabets 
+. A variable name cannot start with a number 
+  keywords cannot be used as variable names
+  blank spaces are not allowed in variable name 
+  special characters are not allowed in variable names , except underscore(_) and dollar sign($)
+
+ */
+
+// var a = 10;
+// var A = 20;
+
+// var name = "";
+
+/*
+var
+const
+final
+if
+switch 
+void 
+ */
+
+// var var1 = 10;
+
+// var first_name = "flutter";
+
+var _name = "flutter";
+
+
+// it is a good habit to use camel case for variable names 
+
+var firstName = "";
